3939c4d2e249d02859c2838d57b6b44613b6651d
jillytot <jill@aylogames.com>
2014-02-15 15:49:45 -0800

    all 8 players seem to be functioning now!

--- Assets/Scripts/playerControls.cs
+++ Assets/Scripts/playerControls.cs
@@ -66,84 +66,84 @@ public class playerControls  {
 		case playerNum.PLAYER3:
 			//print ("this is player 3!!");
 
-			controls.horizontal = "2pHorizontal";
-			controls.vertical = "2pVertical";
+			controls.horizontal = "3pHorizontal";
+			controls.vertical = "3pVertical";
 			
-			controls.fire1 = "2pFire1";
-			controls.fire2 = "2pFire2";
+			controls.fire1 = "3pFire1";
+			controls.fire2 = "3pFire2";
 			controls.fire3 = "2pFire3";
 			
-			controls.jump = "2pJump";
+			controls.jump = "3pJump";
 
 			break;
 
 		case playerNum.PLAYER4:
 			//print ("this is player 4!!");
 
-			controls.horizontal = "2pHorizontal";
-			controls.vertical = "2pVertical";
+			controls.horizontal = "4pHorizontal";
+			controls.vertical = "4pVertical";
 			
-			controls.fire1 = "2pFire1";
-			controls.fire2 = "2pFire2";
+			controls.fire1 = "4pFire1";
+			controls.fire2 = "4pFire2";
 			controls.fire3 = "2pFire3";
 			
-			controls.jump = "2pJump";
+			controls.jump = "4pJump";
 
 			break;
 
 		case playerNum.PLAYER5:
 			//print ("this is player 5!!");
 
-			controls.horizontal = "2pHorizontal";
-			controls.vertical = "2pVertical";
+			controls.horizontal = "5pHorizontal";
+			controls.vertical = "5pVertical";
 			
-			controls.fire1 = "2pFire1";
-			controls.fire2 = "2pFire2";
+			controls.fire1 = "5pFire1";
+			controls.fire2 = "5pFire2";
 			controls.fire3 = "2pFire3";
 			
-			controls.jump = "2pJump";
+			controls.jump = "5pJump";
 
 			break;
 
 		case playerNum.PLAYER6:
 			//print ("this is player 6!!");
 
-			controls.horizontal = "2pHorizontal";
-			controls.vertical = "2pVertical";
+			controls.horizontal = "6pHorizontal";
+			controls.vertical = "6pVertical";
 			
-			controls.fire1 = "2pFire1";
-			controls.fire2 = "2pFire2";
+			controls.fire1 = "6pFire1";
+			controls.fire2 = "6pFire2";
 			controls.fire3 = "2pFire3";
 			
-			controls.jump = "2pJump";
+			controls.jump = "6pJump";
 
 			break;
 
 		case playerNum.PLAYER7:
 			//print ("this is player 7!!");
 
-			controls.horizontal = "2pHorizontal";
-			controls.vertical = "2pVertical";
+			controls.horizontal = "7pHorizontal";
+			controls.vertical = "7pVertical";
 			
-			controls.fire1 = "2pFire1";
-			controls.fire2 = "2pFire2";
+			controls.fire1 = "7pFire1";
+			controls.fire2 = "7pFire2";
 			controls.fire3 = "2pFire3";
 			
-			controls.jump = "2pJump";
+			controls.jump = "7pJump";
 
 			break;
 
 		case playerNum.PLAYER8:
 			Debug.Log ("this is player 8!!");
 
-			controls.horizontal = "2pHorizontal";
-			controls.vertical = "2pVertical";
+			controls.horizontal = "8pHorizontal";
+			controls.vertical = "8pVertical";
 			
-			controls.fire1 = "2pFire1";
-			controls.fire2 = "2pFire2";
+			controls.fire1 = "8pFire1";
+			controls.fire2 = "8pFire2";
 			controls.fire3 = "2pFire3";
 			
-			controls.jump = "2pJump";
+			controls.jump = "8pJump";
 
 			break;
 
