454bef8195fd56ccdf094287c41797afd2633e24
jillytot <jill@aylogames.com>
2014-02-15 18:39:32 -0800

    logo splash screen now automatically loads next level.

---
+++ Assets/loadNext.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadNext : MonoBehaviour {
+
+	public float loadTime = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+
+		StartCoroutine("loadTimer");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator loadTimer () {
+
+		yield return new WaitForSeconds(loadTime);
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
