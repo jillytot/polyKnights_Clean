d9a525c0a4377e90089304dcf4c8ee343510d0a9
jillytot <jill@aylogames.com>
2014-02-17 09:59:56 -0800

    Added Title Scene

--- Assets/Scripts/flickerGraphic.cs
+++ Assets/Scripts/flickerGraphic.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class flickerGraphic : MonoBehaviour {
 
 	public bool flick = false;
+	public bool keepFlickering;
+	public float flickerRate;
 
 	// Use this for initialization
 	void Start () {
@@ -24,8 +26,16 @@ public class flickerGraphic : MonoBehaviour {
 
 	IEnumerator flickRate () {
 		
-		yield return new WaitForSeconds (0.1f);
+		yield return new WaitForSeconds (flickerRate);
 		this.gameObject.renderer.enabled = true;
+		if (keepFlickering == true) {
+
+			flick = false;
+
+		} else {
+
 		flick = true;
+
+		}
 	}
 }
---
+++ Assets/flickerMaterial.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flickerMaterial : MonoBehaviour {
+
+	GameObject myMat;
+	bool flickerMat;
+
+	// Use this for initialization
+	void Start () {
+
+		myMat = this.gameObject;
+		flickerMat = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (flickerMat == true) {
+
+			myMat.renderer.enabled = false;
+			flickerMat = false;
+
+		} 
+
+		if ( flickerMat == false) {
+
+			myMat.renderer.enabled = true;
+			flickerMat = true;
+		}
+	}
+}
