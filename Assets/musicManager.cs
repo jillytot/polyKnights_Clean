869aa4c6fcde788141843527a026d4a0b30178f2
jillytot <jill@aylogames.com>
2014-02-17 20:51:19 -0800

    Fixed up more UI stuff, Started work on Audio Manager stuff

---
+++ Assets/musicManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class musicManager : MonoBehaviour {
+
+	public AudioClip[] mySongs;
+	AudioSource playSong;
+
+	void Awake () {
+
+		DontDestroyOnLoad(this.gameObject);
+		playSong = this.gameObject.GetComponent<AudioSource>();
+
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Application.loadedLevelName == "2p_Test") {
+
+			Debug.Log("Play a new song yo!");
+
+		}
+	}
+}
