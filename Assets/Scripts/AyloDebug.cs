d106e137de226d7ddfa709a5f30544e221a165fa
Jesper Lehmann Poulsen <jesperlp@gmail.com>
2014-05-18 02:55:07 +0200

    Added simple Asset method so the game can help debug itself.

---
+++ Assets/Scripts/AyloDebug.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class AyloDebug {
+	public static void Assert(bool condition) {
+		if(!condition)
+			UnityEngine.Debug.LogError("Assertion failed!\n" + System.Environment.StackTrace);
+	}
+}
